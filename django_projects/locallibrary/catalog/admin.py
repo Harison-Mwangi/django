68579b01390d3115f810d09a593f3523f0772e84
Harison Mwangi <harrisonmwangi288@gmail.com>
2020-05-22 23:11:50 +0300

    Sectioning the layout of the BookInstance detail-view

--- django_projects/locallibrary/catalog/admin.py
+++ django_projects/locallibrary/catalog/admin.py
@@ -25,3 +25,12 @@ class BookAdmin(admin.ModelAdmin):
 class BookInstanceAdmin(admin.ModelAdmin):
     list_display = ('book', 'status', 'due_back', 'id')
     list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
