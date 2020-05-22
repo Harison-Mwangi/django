34b4916c1a514359a9b98d97dc53190dc9344690
Harison Mwangi <harrisonmwangi288@gmail.com>
2020-05-22 23:29:45 +0300

    Inline editing of associated records
    Used a BooksInstanceInline class that inherits from admin.TabularInline to show book instances of a particular book row by row

--- django_projects/locallibrary/catalog/admin.py
+++ django_projects/locallibrary/catalog/admin.py
@@ -15,10 +15,15 @@ class AuthorAdmin(admin.ModelAdmin):
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
 
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra=0
+
 # Register the Admin classes for Book using the decorator
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre', 'language')
+    inlines = [BooksInstanceInline]
 
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance) 
