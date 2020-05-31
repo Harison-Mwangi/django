472091926f341a219f5a4b787f8951488cce874d
Harison Mwangi <harrisonmwangi288@gmail.com>
2020-05-31 21:21:25 +0300

    Added URL configuration for renewing books and changed the URL conf name for all borrowed books list

--- django_projects/locallibrary/catalog/urls.py
+++ django_projects/locallibrary/catalog/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     # path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
     re_path(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
-    path('borrowed/', views.LoanedBooksListView.as_view(), name='borrowed-books'),
+    path('borrowed/', views.LoanedBooksListView.as_view(), name='all-borrowed'),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
 ]
