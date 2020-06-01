96745d8687f3dd8849ba94485d99492318d97b2b
Harison Mwangi <harrisonmwangi288@gmail.com>
2020-06-01 21:15:51 +0300

    Removed the initial value for death date in the Author Create view

--- django_projects/locallibrary/catalog/views.py
+++ django_projects/locallibrary/catalog/views.py
@@ -116,7 +116,6 @@ def renew_book_librarian(request, pk):
 class AuthorCreate(CreateView):
     model = Author
     fields = '__all__'
-    initial = {'date_of_death': '05/01/2020'}
 
 class AuthorUpdate(UpdateView):
     model = Author
