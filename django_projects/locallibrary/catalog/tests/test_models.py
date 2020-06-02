70ba4434f4698fa154f2ba9a38f328bf88cbdc71
Harison Mwangi <harrisonmwangi288@gmail.com>
2020-06-02 20:05:32 +0300

    Refactored tests for Author, Genre and Language into labels, max length and custom class methods subgroups

--- django_projects/locallibrary/catalog/tests/test_models.py
+++ django_projects/locallibrary/catalog/tests/test_models.py
@@ -6,8 +6,9 @@ class AuthorModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         # Set up non-modified objects used by all test methods
-        Author.objects.create(first_name='Big', last_name='Bob')
+        Author.objects.create(first_name='Al', last_name='Sweigart')
 
+    # labels
     def test_first_name_label(self):
         author = Author.objects.get(id=1)
         field_label = author._meta.get_field('first_name').verbose_name
@@ -28,6 +29,7 @@ class AuthorModelTest(TestCase):
         field_label = author._meta.get_field('date_of_death').verbose_name
         self.assertEquals(field_label, 'Died')
 
+    # max length
     def test_first_name_max_length(self):
         author = Author.objects.get(id=1)
         max_length = author._meta.get_field('first_name').max_length
@@ -38,6 +40,7 @@ class AuthorModelTest(TestCase):
         max_length = author._meta.get_field('last_name').max_length
         self.assertEquals(max_length, 100)
 
+    # custom class methods
     def test_object_name_is_last_name_comma_first_name(self):
         author = Author.objects.get(id=1)
         expected_object_name = f'{author.last_name}, {author.first_name}'
@@ -54,15 +57,23 @@ class GenreModelTest(TestCase):
         # Set up non-modified objects used by all test methods
         Genre.objects.create(name='Computer Science')
 
-    def test_object_name(self):
+    # label
+    def test_name_label(self):
         genre = Genre.objects.get(id=1)
-        expected_object_name = genre.name
-        self.assertEquals(expected_object_name, str(genre))
-
+        field_label = genre._meta.get_field('name').verbose_name
+        self.assertEquals(field_label, 'name')
+    
+    # max length
     def test_name_max_length(self):
         genre = Genre.objects.get(id=1)
         max_length = genre._meta.get_field('name').max_length
         self.assertEquals(max_length, 200)
+
+    # custom class method
+    def test_object_name(self):
+        genre = Genre.objects.get(id=1)
+        expected_object_name = genre.name
+        self.assertEquals(expected_object_name, str(genre))
 
 class LanguageModelTest(TestCase):
     @classmethod
@@ -70,12 +81,20 @@ class LanguageModelTest(TestCase):
         # Set up non-modified objects used by all test methods
         Language.objects.create(name='English')
 
-    def test_object_name(self):
+    # label
+    def test_name_label(self):
         language = Language.objects.get(id=1)
-        expected_object_name = language.name
-        self.assertEquals(expected_object_name, str(language))
+        field_label = language._meta.get_field('name').verbose_name
+        self.assertEquals(field_label, 'name')
 
+    # max length
     def test_name_max_length(self):
         language = Language.objects.get(id=1)
         max_length = language._meta.get_field('name').max_length
         self.assertEquals(max_length, 200)
+
+    # custom class method
+    def test_object_name(self):
+        language = Language.objects.get(id=1)
+        expected_object_name = language.name
+        self.assertEquals(expected_object_name, str(language))
